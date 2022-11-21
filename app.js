6ed4cd1006240c1774769ff5dd72c624634d81d2
Luigi <luigi.ciuffi@hotmail.com>
2022-11-21 16:49:46 -0300

    rota 404 not found

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
 //middleware
 app.use(express.static("./views"));
@@ -10,12 +11,7 @@ app.use(express.json());
 
 //routes
 app.use("/api/v1/tasks", tasks);
-
-// app.get('/api/v1/tasks')         - get all the tasks
-// app.post('/api/v1/tasks')        - create a new task
-// app.get('/api/v1/tasks/:id')     - get a single task
-// app.patch('/api/v1/tasks/:id')   - update task
-// app.delete('/api/v1/tasks/:id')  - delete task
+app.use(notFound);
 
 const port = 3000;
 
--- controllers/tasks.js
+++ controllers/tasks.js
@@ -4,6 +4,14 @@ const getAllTasks = async (req, res) => {
     try {
         const tasks = await Task.find({});
         res.status(200).json({ tasks: tasks });
+
+        //Response options
+
+        // res.status(200).json({ tasks, amount: tasks.lenghth });
+        // res.status(200).json({
+        //     status: "success",
+        //     data: { tasks, nbHits: tasks.length },
+        // });
     } catch (error) {
         res.status(500).json({ msg: error });
     }
